6c88671498c036fafd1a5d7eb9127c1a88435958
Michael Mellor <michael.reuben.mellor@gmail.com>
2024-06-10 14:36:22 +0100

    feat: improvements

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import pickle
 import os
+import logging
+import argparse
+import json
+
+fridge = None
+recipes = None
 
 class Recipe:
-    def __init__(self):
-        self.name = input("\nName of meal:\n")
-        self.ingredients = {}
-        ingredient = add_ingredient()
-        while ingredient.casefold() != "finished":
-            self.ingredients[ingredient] = int_validator("How much? (Standard units = g, ml, pieces)\n")
+    def __init__(self, name=None, ingredients=None, servings=None):
+        if name and ingredients and servings:
+            self.name = name 
+            self.ingredients = ingredients 
+            self.servings = servings
+        else:
+            self.name = input("\nName of meal:\n")
+            self.ingredients = {}
             ingredient = add_ingredient()
-        self.servings = int_validator("How many does it serve?\n")
+            while ingredient.casefold() != "finished":
+                self.ingredients[ingredient] = int_validator("How much? (Standard units = g, ml, pieces)\n")
+                ingredient = add_ingredient()
+            self.servings = int_validator("How many does it serve?\n")
     
     def print_recipe(self):
         print("\nRecipe:", self.name)
@@ -18,27 +29,28 @@ class Recipe:
             print(f"{num}. {ingredient}")
         print("Serves", self.servings)
 
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "ingredients": self.ingredients,
+            "servings": self.servings
+        }
+
 class Fridge:
-    def __init__(self):
-        self.ingredients = {}
+    def __init__(self, ingredients={}):
+        self.ingredients = ingredients
         
 
     def print_fridge(self):
         print("Ingredients:")
         for num, ingredient in self.ingredients.items():
             print(f"{num}. {ingredient}")
-            
-if os.path.getsize('recipes.pkl') == 0:
-    recipes = []
-else:
-    with open('recipes.pkl', 'rb') as f:
-        recipes = pickle.load(f)
 
-if os.path.getsize('fridge.pkl') == 0:
-    fridge = Fridge()
-else:
-    with open('fridge.pkl', 'rb') as f:
-        fridge = pickle.load(f)
+    def to_dict(self):
+        return {
+            "ingredients": self.ingredients
+        }
+
 
 def int_validator(question):
     user_input = input(question)
@@ -83,15 +95,14 @@ def ingredient_validator(ingredient):
 
         
 def fridge_writer():
-    with open('fridge.pkl', 'wb') as f:
+    with open('data/fridge.pkl', 'wb') as f:
         pickle.dump(fridge, f) 
 
 def recipe_writer():
-    with open('recipes.pkl', 'wb') as f:
+    with open('data/recipes.pkl', 'wb') as f:
         pickle.dump(recipes, f)
         
 def add_fridge():
-    
     if len(fridge.ingredients) == 0:
         print("\nFridge is empty :(")
     else:
@@ -190,8 +201,19 @@ def remove_recipe():
 
 
 def menu():
-    print("\n-------------------------------------------")
-    user_input = int_validator("What would you like to do today?\n1. Find what recipes are available\n2. Update the fridge inventory\n3. Add a new recipe\n4. Remove a recipe\n5. View all recipes and fridge inventory\n6. End program\n-------------------------------------------\n")
+    user_input = int_validator("""
+-------------------------------------------
+What would you like to do today?
+1. Find what recipes are available
+2. Update the fridge inventory
+3. Add a new recipe
+4. Remove a recipe
+5. View all recipes and fridge inventory
+6. Export to JSON
+7. End program
+-------------------------------------------
+    """
+    )
     
     if user_input == 1:
         recipe_checker()
@@ -214,11 +236,59 @@ def menu():
             print("Fridge Inventory:\n")
             fridge.print_fridge()
             menu()
-    elif user_input == 6:
+    elif user_input == 6: 
+        file_name = input("Please provide a file name \n")
+        if ".json" not in file_name:
+            file_name += ".json"
+        data_file_content = {
+            "Recipes": list(map(lambda recipe: recipe.to_dict(), recipes)),
+            "FridgeInventory": fridge.to_dict()
+        }
+        with open(file_name, "w") as data_file:
+            data_file.write(json.dumps(data_file_content, indent=4))
+    elif user_input == 7:
         print("Closing program...")
         return
     else:
         print("Invalid input\n")
         menu()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="process recipe program arguments")
+    parser.add_argument('--data-file', type=str, help='path to the data file providing this value will override any existing local data')
+    args = parser.parse_args()
     
-menu()    
+    global fridge
+    global recipes
+
+    if args.data_file:
+        print(f"loading data from {args.data_file}")
+        with open(args.data_file, "r") as f:
+            data_file_content = json.loads(f.read())
+        fridge = Fridge(ingredients=data_file_content["FridgeInventory"]["ingredients"])
+        recipes = list(map(lambda recipe: Recipe(name=recipe["name"], ingredients=recipe["ingredients"], servings=recipe["servings"]), data_file_content["Recipes"]))
+        fridge_writer()
+        recipe_writer()
+        menu()
+    else: 
+        if not os.path.exists('data/recipes.pkl') or os.path.getsize('data/recipes.pkl') == 0:
+            logging.info("no local recipe data found")
+            recipes = []
+        else:
+            logging.info("recipe data detected")
+            with open('data/recipes.pkl', 'rb') as f:
+                recipes = pickle.load(f)
+
+        if not os.path.exists('data/fridge.pkl') or os.path.getsize('data/fridge.pkl') == 0:
+            logging.info("no local fridge data found")
+            fridge = Fridge()
+        else:
+            logging.info("fridge data detected")
+            with open('data/fridge.pkl', 'rb') as f:
+                fridge = pickle.load(f)
+
+        menu()
+
+if __name__ == "__main__":
+    main()
